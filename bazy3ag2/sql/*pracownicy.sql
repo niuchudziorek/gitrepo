543985b7fd89b02574a507f7f0acc0b8438e2222
niuchudziorek <niuchuman@onet.pl>
2017-10-11 12:49:15 +0200

    Podstawy

---
+++ bazy3ag2/sql/*pracownicy.sql
@@ -0,0 +1,23 @@
+CREATE TABLE premia (
+    id VARCHAR(20) PRIMARY KEY,
+    PREMIA NUMERIC
+);
+
+CREATE TABLE dzial (
+    id INTEGER PRIMARY KEY,
+    nazwa VARCHAR(20),
+    siedziba VARCHAR(20)
+);
+
+CREATE TABLE pracownicy (
+    id VARCHAR(20) PRIMARY KEY,
+    nazwisko VARCHAR(20),
+    imie VARCHAR(20),
+    stanowsiko VARCHAR(20),
+    data_zatrudnienia VARCHAR(23),
+    placa NUMERIC,
+    premia NUMERIC,
+    id_dzial INTEGER,
+    FOREIGN KEY(stanowisko) REFERENCES premia(id),
+    FOREIGN KEY(id_dzial) REFERENCES dzial(id)
+);
