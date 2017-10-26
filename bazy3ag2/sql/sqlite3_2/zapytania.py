75da3d57d6d91c25d03d178d98703abf543ff9c6
niuchudziorek <niuchuman@onet.pl>
2017-10-26 08:09:03 +0200

    Zadania_git

---
+++ bazy3ag2/sql/sqlite3_2/zapytania.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  baza_sql.py
+
+import sqlite3
+
+def wyniki(dane):
+    for rekord in dane:
+        print(tuple(rekord))
+
+
+def kw_a(cur):
+    print('1)')
+    cur.execute("""
+        SELECT Imie, Nazwisko, Klasa
+        FROM tbUczniowie, tbKlasy
+        WHERE tbUczniowie.KlasaID = tbKlasy.IDKlasy
+        AND Klasa = '1A'
+    """)
+    wyniki(cur.fetchall())
+     
+def kw_aa(cur):
+    print('2)')
+    cur.execute("""
+        SELECT Imie, Nazwisko, MAX(EgzHum)
+        FROM tbUczniowie
+    """)
+    wyniki(cur.fetchall())
+
+def kw_aaa(cur):
+    print('3)')
+    cur.execute("""
+        SELECT Klasa, AVG(EgzMat)
+        FROM tbUczniowie, tbKlasy
+        WHERE tbUczniowie.KlasaID = tbKlasy.IDKlasy
+        AND Klasa = '1A'
+    """)
+    wyniki(cur.fetchall())
+
+def kw_aaaa(cur):
+    print('4)')
+    cur.execute("""
+        SELECT Ocena
+        FROM tbUczniowie, tbOceny
+        WHERE tbUczniowie.IDUcznia = tbOceny.UczenID
+        AND Imie = 'Dorota'
+        And Nazwisko = 'Nowak'
+    """)
+    wyniki(cur.fetchall())
+
+def kw_aaaaa(cur):
+    print('5)')
+    cur.execute("""
+        SELECT AVG(Ocena)
+        FROM tbOceny, tbPrzedmioty
+        WHERE tbOceny.PrzedmiotID = tbPrzedmioty.IDPrzedmiotu
+        AND tbPrzedmioty.Przedmiot = 'fizyka'
+        AND Datad > '2012-10-01'
+        AND Datad < '2012-10-31'
+    """)
+    wyniki(cur.fetchall())
+
+
+def main(args):
+    con = sqlite3.connect('szkola.db')
+    cur = con.cursor() #utworzenie kursora
+    
+    kw_a(cur)
+    kw_aa(cur)
+    kw_aaa(cur)
+    kw_aaaa(cur)
+    kw_aaaaa(cur)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
